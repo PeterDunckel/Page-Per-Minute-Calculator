e4f6133fbc8c29bfc7c4daa2ab288d1565c89948
PeterDunckel <peterdunckel94@yahoo.com>
2014-12-19 10:35:35 -0800

    New project

---
+++ PPMCalc/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.butCalculate = new System.Windows.Forms.Button();
+            this.txtPageCount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTimeCount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPPM = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIPM = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.butStop = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.ClearBut = new System.Windows.Forms.Button();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape3 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.SuspendLayout();
+            // 
+            // butCalculate
+            // 
+            this.butCalculate.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butCalculate.Location = new System.Drawing.Point(129, 95);
+            this.butCalculate.Name = "butCalculate";
+            this.butCalculate.Size = new System.Drawing.Size(122, 37);
+            this.butCalculate.TabIndex = 0;
+            this.butCalculate.Text = "Calculate";
+            this.butCalculate.UseVisualStyleBackColor = true;
+            this.butCalculate.Click += new System.EventHandler(this.butCalculate_Click);
+            // 
+            // txtPageCount
+            // 
+            this.txtPageCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPageCount.Location = new System.Drawing.Point(142, 31);
+            this.txtPageCount.MaxLength = 7;
+            this.txtPageCount.Name = "txtPageCount";
+            this.txtPageCount.Size = new System.Drawing.Size(100, 26);
+            this.txtPageCount.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pages Scanned:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Time:";
+            // 
+            // txtTimeCount
+            // 
+            this.txtTimeCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTimeCount.Location = new System.Drawing.Point(142, 63);
+            this.txtTimeCount.MaxLength = 6;
+            this.txtTimeCount.Name = "txtTimeCount";
+            this.txtTimeCount.Size = new System.Drawing.Size(100, 26);
+            this.txtTimeCount.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "PPM:";
+            // 
+            // txtPPM
+            // 
+            this.txtPPM.Enabled = false;
+            this.txtPPM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPPM.Location = new System.Drawing.Point(142, 154);
+            this.txtPPM.Name = "txtPPM";
+            this.txtPPM.Size = new System.Drawing.Size(100, 26);
+            this.txtPPM.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "IPM:";
+            // 
+            // txtIPM
+            // 
+            this.txtIPM.Enabled = false;
+            this.txtIPM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIPM.Location = new System.Drawing.Point(142, 190);
+            this.txtIPM.Name = "txtIPM";
+            this.txtIPM.Size = new System.Drawing.Size(100, 26);
+            this.txtIPM.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(417, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 49);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(470, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Stop Watch";
+            // 
+            // butStop
+            // 
+            this.butStop.BackColor = System.Drawing.Color.Red;
+            this.butStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butStop.Location = new System.Drawing.Point(486, 38);
+            this.butStop.Name = "butStop";
+            this.butStop.Size = new System.Drawing.Size(62, 49);
+            this.butStop.TabIndex = 13;
+            this.butStop.Text = "Stop";
+            this.butStop.UseVisualStyleBackColor = false;
+            this.butStop.Click += new System.EventHandler(this.butStop_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(554, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(62, 49);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // ClearBut
+            // 
+            this.ClearBut.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ClearBut.Location = new System.Drawing.Point(129, 245);
+            this.ClearBut.Name = "ClearBut";
+            this.ClearBut.Size = new System.Drawing.Size(122, 37);
+            this.ClearBut.TabIndex = 15;
+            this.ClearBut.Text = "Clear All";
+            this.ClearBut.UseVisualStyleBackColor = true;
+            this.ClearBut.Click += new System.EventHandler(this.ClearBut_Click);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape3,
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(676, 294);
+            this.shapeContainer1.TabIndex = 16;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 382;
+            this.lineShape1.X2 = 643;
+            this.lineShape1.Y1 = 32;
+            this.lineShape1.Y2 = 32;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 92;
+            this.lineShape2.X2 = 293;
+            this.lineShape2.Y1 = 143;
+            this.lineShape2.Y2 = 143;
+            // 
+            // lineShape3
+            // 
+            this.lineShape3.Name = "lineShape3";
+            this.lineShape3.X1 = 90;
+            this.lineShape3.X2 = 291;
+            this.lineShape3.Y1 = 231;
+            this.lineShape3.Y2 = 231;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 294);
+            this.Controls.Add(this.ClearBut);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.butStop);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtIPM);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPPM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTimeCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPageCount);
+            this.Controls.Add(this.butCalculate);
+            this.Controls.Add(this.shapeContainer1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Peter\'s Multifunctional Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butCalculate;
+        private System.Windows.Forms.TextBox txtPageCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTimeCount;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPPM;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIPM;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button butStop;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button ClearBut;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape3;
+    }
+}
+
---
+++ PPMCalc/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private int CheckIntParam(string value, string message)
+        {
+            int val;
+            bool isGood = Int32.TryParse(value, out val);
+            if (!isGood)
+            {
+                throw new Exception(message);
+            }
+            return val;
+        }
+
+        private double CheckDoubleParam(string value, string message)
+        {
+            double val;
+            bool isGood = double.TryParse(value, out val);
+            if (!isGood)
+            {
+                throw new Exception(message);
+            }
+            return val;
+        }
+
+        private void Calculate()
+        {
+            try
+            {
+                int pageCount = CheckIntParam(txtPageCount.Text, "Page Count must be an integer");
+                Double timeOfScan = CheckDoubleParam(txtTimeCount.Text, "Time must be a double");
+
+                double pagesPerMinute = 60.0 * pageCount / timeOfScan;
+
+                double imagesPerMinute = 2 * 60.0 * pageCount / timeOfScan;
+
+                txtPPM.Text = Math.Round(pagesPerMinute, 1).ToString();
+                txtIPM.Text = Math.Round(imagesPerMinute, 1).ToString();
+            }
+            catch (Exception ex)
+            {
+                ReportError(ex);
+            }
+        }
+
+        private void butCalculate_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (txtTimeCount.Text == "")
+            {
+                txtTimeCount.Text = "0";
+            }
+
+            try
+            {
+                Double timeOfScan = CheckDoubleParam(txtTimeCount.Text, "Time must be a double");
+                txtTimeCount.Enabled = false;
+                timer1.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            txtTimeCount.Text = (CheckDoubleParam(txtTimeCount.Text, "bad double") + .1).ToString();
+        }
+
+        private void butStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            txtTimeCount.Enabled = true;
+
+            if (txtPageCount.Text != "")
+            {
+
+                Calculate();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtTimeCount.Text = "";
+        }
+
+        private void ClearBut_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (timer1.Enabled)
+                {
+                    throw new Exception("Can't clear while clock is running!");
+                }
+                txtTimeCount.Text = "";
+                txtPageCount.Text = "";
+                txtPPM.Text = "";
+                txtIPM.Text = "";
+            }
+            catch (Exception ex)
+            {
+                ReportError(ex);
+            }
+        }
+
+        private void ReportError(Exception ex)
+        {
+            MessageBox.Show("An error occurred: " + ex.Message);
+           
+        }
+
+       
+
+      
+
+        
+
+        
+    }
+}
